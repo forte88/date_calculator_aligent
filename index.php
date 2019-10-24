8d328c35b6ca26a9970f2e98450bd8cdb25336a4
forte88 <davidparry610@gmail.com>
2019-10-24 22:11:50 +1030

    included interface

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 require 'vendor/autoload.php';
 
 $app = new Slim\App();
 
 // Define app routes
-$app->get('/hello/{name}', function ($request, $response, $args) {
+$app->get('/hello/{name}', function (Request $request,Response $response, array $args) {
     return $response->write("Hello " . $args['name']);
 });
 
