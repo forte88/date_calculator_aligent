9b82e1f5f8728f7a06c741d9fd9301fbac3a4728
forte88 <davidparry610@gmail.com>
2019-11-03 18:47:08 +1030

    forgot to add the server in last commit refer to last comments

---
+++ src/DateCalc/Services/CalculateDaysService.php
@@ -0,0 +1,105 @@
+<?php
+namespace DateCalc\Services;
+
+use DateTime;
+use Exception;
+
+class CalculateDaysService
+{
+
+    /**Converts start and end stings into datetime objects
+     *Calculates the difference in unixtime seconds
+     * @param $start
+     * @param $end
+     * @return int as seconds
+     * @throws Exception
+     */
+    private function intervalBetweenDates($start, $end){
+        $start = new DateTime("$start");
+        $end = new DateTime("$end");
+        $interval = $end->getTimestamp() - $start->getTimestamp();
+        return $interval;
+    }
+
+    private function weekDayCalc($starttime, $endtime){
+        $days = [];
+        $dayCount = 0;
+        $weekDayCount = 0;
+
+        $start = new DateTime("$starttime");
+        $end = new DateTime("$endtime");
+
+        for ($i = $start; $i <= $end; $i->modify('+1 day')){
+            $days[] = $i->format('N');
+            $dayCount++;
+        }
+        foreach ($days as $day){
+            if (!($day == 6 || $day == 7)){
+                $weekDayCount++;
+            }
+        }
+
+        $weekDay = ($dayCount - $weekDayCount) * (24*60*60);
+        $weekDay =  (strtotime($endtime)-strtotime($starttime))-$weekDay;
+
+        return $weekDay;
+    }
+
+    /**Converts seconds into desired format in whole numbers rounded down.
+     * All formatted in Years, Days, Hours, Minutes and Seconds
+     * @param $timeInSeconds
+     * @param $format
+     * @return array
+     */
+    private function dateConverter($timeInSeconds, $format){
+        $payload = $timeInSeconds;
+        if ($format == 'd'){
+            $payload = ['days' => floor($timeInSeconds / (24 *3600))];
+        }else if($format == 'y'){
+            $payload = ['years'=> floor($timeInSeconds / ((24 *3600) * 365))];
+        }else if($format == 'h'){
+            $payload = ['hours' => floor($timeInSeconds / 3600)];
+        }else if($format == 'm'){
+            $payload = ['minutes' => floor($timeInSeconds / 60)];
+        }else if ($format == 's'){
+            $payload = ['seconds' => $timeInSeconds];
+        }else if ($format == 'a'){
+            $years=0;
+            $days = $timeInSeconds / (24 * 3600);
+            if($days > 365){
+                $years =  $days / 365;
+                $days = $days % 365;
+            }
+            $remainder = $timeInSeconds % (24 * 3600);
+            $hour = $remainder / 3600;
+            $remainder %= 3600;
+            $minute = $remainder / 60;
+            $remainder %= 60;
+            $second = $remainder;
+
+            $payload = [
+                'Years' => floor($years),
+                'Days' => floor($days),
+                'Hours' => floor($hour),
+                'Minutes' => floor($minute),
+                'Seconds' => floor($second)
+            ];
+
+        }else{
+            $payload = 0;
+        }
+        return $payload;
+    }
+
+
+    /**
+     * @param $data
+     * @return array
+     * @throws Exception
+     */
+    public function calcDaysService($data){
+        $days = $this->intervalBetweenDates($data['start'],$data['end']);
+        $payload = $this->dateConverter($days, $data['formatted']);
+        return $payload;
+    }
+}
